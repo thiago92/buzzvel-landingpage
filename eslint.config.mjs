5bd0282c40ed1c68e90a1d3bd05ea4ae81c8a4b5
Thiago da Silva de Souza <thiago.dsouza1992@gmail.com>
2025-05-07 09:10:31 -0300

    Commit initial

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "indent": ["error", "tab"],
+      "semi": ["error", "always"],
+    },
+  },
 ];
+
 
 export default eslintConfig;
